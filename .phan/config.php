03aa8ec96d67ad43ba1331a4fdb803dd8be654d2
Dirk Adler <dirk.adler@idealo.de>
2018-05-25 16:19:55 +0200

    add phan config

---
+++ .phan/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "target_php_version" => null,
+    'directory_list' => [
+        'src',
+        'vendor/psr',
+    ],
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+];
