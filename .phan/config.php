d6a2cf2ba1ed926e65f05bf158e1fce43c297df0
Dirk Adler <dirk.adler@idealo.de>
2018-05-25 17:15:12 +0200

    add phan config workaround

--- .phan/config.php
+++ .phan/config.php
@@ -1,12 +1,17 @@
 <?php
 
-return [
-    "target_php_version" => null,
+$config = [
+    'target_php_version' => null,
     'directory_list' => [
         'src',
-        'vendor/psr',
     ],
-    "exclude_analysis_directory_list" => [
-        'vendor/'
-    ],
+    'exclude_analysis_directory_list' => [],
 ];
+
+// workaround for phan on codeclimate
+if (file_exists(dirname(__DIR__) . '/vendor/psr')) {
+    $config['directory_list'] [] = 'vendor/psr';
+    $config['exclude_analysis_directory_list'] [] = 'vendor/';
+}
+
+return $config;
